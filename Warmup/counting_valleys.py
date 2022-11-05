eb6afd6f0acddf414cc9b27b1ad8509f82341b76
Md. Riyadul Hasan <riadulhasancse48@gmail.com>
2022-11-05 11:47:31 +0600

    Create counting_valleys.py

---
+++ Warmup/counting_valleys.py
@@ -0,0 +1,21 @@
+import math
+import os
+import random
+import re
+import sys
+def countingValleys(steps, path):
+    count=0
+    res=0
+    for c in path:
+        if c=='U':
+            count+=1
+        else:
+            count-=1
+        if count==0 and c=='U':
+            res+=1
+    return res
+if __name__ == '__main__':
+    steps = 8 #int(input().strip())
+    path = 'UDDDUDUU' #input()
+    result = countingValleys(steps, path)
+    print(result)
